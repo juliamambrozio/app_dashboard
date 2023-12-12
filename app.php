38949d9ff51a7bb6c7d61d575e7d5fcac76665b6
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-12-12 14:41:17 -0300

    exibindo dados

--- app.php
+++ app.php
@@ -97,16 +97,22 @@ $dashboard = new Dashboard();
 
 $conexao = new Conexao();
 
-$dashboard->__set('data_inicio', '2018-10-01');
-$dashboard->__set('data_fim', '2018-10-31'); //atribuindo valor a variável contida na var dentro da class dashboard
+$competencia = explode('-', $_GET['competencia']);
+$ano = $competencia[0];
+$mes = $competencia[1];
+
+//CONTAGEM DE DIAS
+$dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+
+$dashboard->__set('data_inicio', $ano.'-'.$mes.'-'.'01');
+$dashboard->__set('data_fim', $ano.'-'.$mes.'-'.$dias_do_mes); //atribuindo valor a variável contida na var dentro da class dashboard
 
 
 $bd = new Bd($conexao, $dashboard);
 
 $dashboard->__set('numeroVendas', $bd->getNumeroVendas());
 $dashboard->__set('totalVendas', $bd->getTotalVendas()); //setando valor do totalVendas que fica em dashboard para o valor que fica no bd na function TotalVendas
-print_r($dashboard);
+echo json_encode($dashboard);
 
 
-?>
 ?>
--- script.js
+++ script.js
@@ -27,4 +27,28 @@ $(document).ready(() => {
             $('#pagina').html(data)
         })
     })
+
+    $('#competencia').on('change', e =>{
+        //FAZENDO REQUISIÇÃI SÍNCRONA
+
+        let competencia = $(e.target).val()
+
+        $.ajax({
+            //método, url, dados, sucesso, erro
+            type: 'GET',
+            url: 'app.php',
+            data: 'competencia='+ competencia, //name do campo = valor (urlcoded)
+            dataType: 'json', //passando de formato HTML para JSON (virando um objeto literal)
+            success:dados => {
+                //ATRIBUINDO VALORES AOS CAMPOS
+            
+                $('#numeroVendas').html(dados.numeroVendas)
+                $('#totalVendas').html(dados.totalVendas)
+            },
+            error: erro => {
+                console.log(erro)
+            }
+
+        })
+    })
 })
